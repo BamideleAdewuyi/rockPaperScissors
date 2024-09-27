4f4c1049ff409b06dffa7c9e5e11284b48c39542
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2024-09-27 02:12:45 +0100

    Wrote getHumanChoice function. It takes user input of 1, 2 or 3 and returns their choice of rock, paper or scissors respectively.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -16,5 +16,22 @@ function getComputerChoice() {
 
 // Function for getting human's choice
 function getHumanChoice() {
-    
-} 
+    let num = prompt("Enter 1 for rock, 2 for paper or 3 for scissors");
+    console.log(num)
+    if (num == 1) {
+        choice = "Rock";
+    }
+    else if (num == 2) {
+        choice = "Paper";
+    }
+    else if (num == 3) {
+        choice = "Scissors";
+    }
+    else {
+        choice = null;
+        alert("You did not make a choice!")
+    }
+    return choice;
+}
+
+console.log(getHumanChoice())
