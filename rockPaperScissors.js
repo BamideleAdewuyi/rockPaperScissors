71fc86eeb0d6b83047c6cdb1f76bf8d8f6de98f1
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2024-09-27 01:47:30 +0100

    Wrote getComputerChoice function. It now prints a random choice of rock, paper or scissors

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,4 +1,17 @@
 // Create function for computer's choice
 function getComputerChoice() {
-    
+    let num = Math.random();
+
+    if (num <= 1/3) {
+        choice = "Rock";
+    }
+    else if (num > 1/3 && num <= 2/3) {
+        choice = "Paper";
+    }
+    else {
+        choice = "Scissors";
+    }
+    console.log(choice)
 }
+
+getComputerChoice()
