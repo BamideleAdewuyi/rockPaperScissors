4056cdf6d4659a8ed27fe48c3661f72def2c0a3f
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2024-09-30 21:30:10 +0100

    In rockPaperScissors.js, added if statements to playRound function

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -16,8 +16,8 @@ function getComputerChoice() {
 
 // Function for getting human's choice
 function getHumanChoice() {
-    let choice = prompt("Enter rock, paper or scissors");
-    return choice.toLowerCase();
+    let choice = prompt("Enter rock, paper or scissors").toLowerCase();
+    return choice;
 }
 
 
@@ -27,6 +27,33 @@ let computerScore = 0;
 
 // Function for playing a single round
 function playRound(humanChoice, computerChoice) {
+    if (humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("You win! Rock beats scissors");
+        humanChoice++;
+    }
+    else if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log("You lose! Paper beats rock");
+        computerChoice++;
+    }
+    else if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log("You win! Paper beats rock");
+        humanChoice++;
+    }
+    else if (humanChoice == "paper" && computerChoice == "scissors") {
+        console.log("You lose! Scissors beats paper");
+        computerChoice++;
+    }
+    if (humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("You win! Scissors beats paper");
+        humanChoice++;
+    }
+    else if (humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("You lose! Rock beats scissors");
+        computerChoice++;
+    }
+    else if (humanChoice == computerChoice) {
+        console.log('Draw! ${humanChoice} and ${computerChocie} are even.')
+    }
     return
 }
 
