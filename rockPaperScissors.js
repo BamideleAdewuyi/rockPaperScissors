8d531a1171aab36a40c1013293e8dada263025c0
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2024-10-09 22:04:38 +0100

    rockPaperScissors.js: Edited getHumanChoice to show text content prompting selection. In playGame, moved while loop to end of function

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,12 +1,17 @@
 // Create body object to add buttons and results to
 const body = document.querySelector("body");
-// Prompt div
+
+// Selection p
 const select = document.createElement("p");
-select.textContent = "Select rock, paper or scissors";
 body.appendChild(select);
+
 // Results div
 const results = document.createElement("div");
 body.appendChild(results);
+
+// Paragraph for round message
+const roundMsg = document.createTextNode("");
+results.appendChild(roundMsg);
 
 // Function for computer's choice
 function getComputerChoice() {
@@ -26,8 +31,8 @@ function getComputerChoice() {
 
 // Function for getting human's choice
 function getHumanChoice() {
-    let choice = prompt("Enter rock, paper or scissors").toLowerCase();
-    return choice;
+    select.textContent = "Select rock, paper or scissors";
+    return;
 }
 
 
@@ -45,49 +50,34 @@ function playGame() {
     // Function for playing a single round
     function playRound(humanChoice, computerChoice) {
         if (humanChoice == "rock" && computerChoice == "scissors") {
-            results.textContent = "You win! Rock beats scissors";
+            roundMsg.textContent = "You win! Rock beats scissors";
             humanScore+=1;
         }
         else if (humanChoice == "rock" && computerChoice == "paper") {
-            results.textContent = "You lose! Paper beats rock";
+            roundMsg.textContent = "You lose! Paper beats rock";
             computerScore+=1;
         }
         else if (humanChoice == "paper" && computerChoice == "rock") {
-            results.textContent = "You win! Paper beats rock";
+            roundMsg.textContent = "You win! Paper beats rock";
             humanScore+=1;
         }
         else if (humanChoice == "paper" && computerChoice == "scissors") {
-            results.textContent = "You lose! Scissors beats paper";
+            roundMsg.textContent = "You lose! Scissors beats paper";
             computerScore+=1;
         }
         if (humanChoice == "scissors" && computerChoice == "paper") {
-            results.textContent = "You win! Scissors beats paper";
+            roundMsg.textContent = "You win! Scissors beats paper";
             humanScore+=1;
         }
         else if (humanChoice == "scissors" && computerChoice == "rock") {
-            results.textContent = "You lose! Rock beats scissors";
+            roundMsg.textContent = "You lose! Rock beats scissors";
             computerScore+=1;
         }
         else if (humanChoice == computerChoice) {
-            results.textContent = `Draw! ${humanChoice} and ${computerChoice} are even.`;
+            roundMsg.textContent = `Draw! ${humanChoice} and ${computerChoice} are even.`;
         }
         return
     }
-    // // Set up while loop to run until a point has been scored 5 times
-    // while ((humanScore + computerScore) < 5) {
-    //     // Create constants for selections
-    //     const humanSelection = getHumanChoice();
-    //     const computerSelection = getComputerChoice();
-    //     // Call play round
-    //     playRound(humanSelection, computerSelection);
-    //     console.log(`Current scores:\nYou: ${humanScore} Computer: ${computerScore}`);
-    // }
-    // if (humanScore > computerScore) {
-    //     console.log("You win!")
-    // }
-    // else {
-    //     console.log("You lost!")
-    // }
 
     // Button for each selection
     const rock = document.createElement("button");
@@ -106,7 +96,23 @@ function playGame() {
     paper.addEventListener("click", playRound("paper", getComputerChoice));
     scissors.addEventListener("click", playRound("scissors", getComputerChoice));
 
-    return
+    // Set up while loop to run until a point has been scored 5 times
+    while ((humanScore + computerScore) < 5) {
+        // Create constants for selections
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        // Call play round
+        playRound(humanSelection, computerSelection);
+        console.log(`Current scores:\nYou: ${humanScore} Computer: ${computerScore}`);
+    }
+    if (humanScore > computerScore) {
+        console.log("You win!")
+    }
+    else {
+        console.log("You lost!")
+    }
+    
+    return;
 }
 
 playGame();
