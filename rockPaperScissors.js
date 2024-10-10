0668344378e855dc8e270eeee968246edd4113ce
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2024-10-10 15:41:28 +0100

    rockPaperScissors.js: Moved playRound out of playGame

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -16,7 +16,36 @@ function getComputerChoice() {
     return choice
 }
 
-
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == "rock" && computerChoice == "scissors") {
+        console.log("You win! Rock beats scissors");
+        humanScore+=1;
+    }
+    else if (humanChoice == "rock" && computerChoice == "paper") {
+        console.log("You lose! Paper beats rock");
+        computerScore+=1;
+    }
+    else if (humanChoice == "paper" && computerChoice == "rock") {
+        console.log("You win! Paper beats rock");
+        humanScore+=1;
+    }
+    else if (humanChoice == "paper" && computerChoice == "scissors") {
+        console.log("You lose! Scissors beats paper");
+        computerScore+=1;
+    }
+    if (humanChoice == "scissors" && computerChoice == "paper") {
+        console.log("You win! Scissors beats paper");
+        humanScore+=1;
+    }
+    else if (humanChoice == "scissors" && computerChoice == "rock") {
+        console.log("You lose! Rock beats scissors");
+        computerScore+=1;
+    }
+    else if (humanChoice == computerChoice) {
+        console.log(`Draw! ${humanChoice} and ${computerChoice} are even.`);
+    
+    return;
+}
 const testBtn = document.createElement("button")
 testBtn.textContent = "Testing";
 body.appendChild(testBtn);
@@ -29,44 +58,16 @@ testBtn.addEventListener("click", () => {
     testFunc();
 });
 
+// 
+
 // Set up game to play for 5 rounds
 function playGame() {
 
     let humanScore = 0;
     let computerScore = 0;
 
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice == "rock" && computerChoice == "scissors") {
-            console.log("You win! Rock beats scissors");
-            humanScore+=1;
-        }
-        else if (humanChoice == "rock" && computerChoice == "paper") {
-            console.log("You lose! Paper beats rock");
-            computerScore+=1;
-        }
-        else if (humanChoice == "paper" && computerChoice == "rock") {
-            console.log("You win! Paper beats rock");
-            humanScore+=1;
-        }
-        else if (humanChoice == "paper" && computerChoice == "scissors") {
-            console.log("You lose! Scissors beats paper");
-            computerScore+=1;
-        }
-        if (humanChoice == "scissors" && computerChoice == "paper") {
-            console.log("You win! Scissors beats paper");
-            humanScore+=1;
-        }
-        else if (humanChoice == "scissors" && computerChoice == "rock") {
-            console.log("You lose! Rock beats scissors");
-            computerScore+=1;
-        }
-        else if (humanChoice == computerChoice) {
-            console.log(`Draw! ${humanChoice} and ${computerChoice} are even.`);
-        }
-        return;
-    };
     
-};
+    };
 
 playGame();
 
