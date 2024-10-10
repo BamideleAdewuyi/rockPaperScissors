085d288a6504abe6c3455383188d5b55e3b3decf
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2024-10-10 16:49:51 +0100

    rockPaperScissors.js: Added event handler for each button

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -39,13 +39,13 @@ function playRound(humanChoice, computerChoice) {
 }
 
 
-// // buttons is a node list. It looks and acts much like an array.
-// const buttons = document.querySelectorAll("button");
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
 
-// // we use the .forEach method to iterate through each button
-// buttons.forEach((button) => {
-//   // and for each one we add a 'click' listener
-//   button.addEventListener("click", playRound(button.id, getComputerChoice));
-// });
-
-// playRound("rock", "scissors")
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    playRound(button.id, getComputerChoice())
+});
+});
