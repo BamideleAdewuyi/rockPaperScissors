e8a3899a1dc8dbcf94472a84e28c0188f4aeb3db
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2024-10-10 17:40:25 +0100

    rockPaperScissors.js: Changed console.log lines to text content changes

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -24,31 +24,31 @@ function playGame() {
 
     function playRound(humanChoice, computerChoice) {
         if (humanChoice == "rock" && computerChoice == "scissors") {
-            console.log("You win! Rock beats scissors");
+            results.textContent = "You win! Rock beats scissors";
             humanScore+=1;
         }
         else if (humanChoice == "rock" && computerChoice == "paper") {
-            console.log("You lose! Paper beats rock");
+            results.textContent = "You lose! Paper beats rock";
             computerScore+=1;
         }
         else if (humanChoice == "paper" && computerChoice == "rock") {
-            console.log("You win! Paper beats rock");
+            results.textContent = "You win! Paper beats rock";
             humanScore+=1;
         }
         else if (humanChoice == "paper" && computerChoice == "scissors") {
-            console.log("You lose! Scissors beats paper");
+            results.textContent = "You lose! Scissors beats paper";
             computerScore+=1;
         }
         if (humanChoice == "scissors" && computerChoice == "paper") {
-            console.log("You win! Scissors beats paper");
+            results.textContent = "You win! Scissors beats paper";
             humanScore+=1;
         }
         else if (humanChoice == "scissors" && computerChoice == "rock") {
-            console.log("You lose! Rock beats scissors");
+            results.textContent = "You lose! Rock beats scissors";
             computerScore+=1;
         }
         else if (humanChoice == computerChoice) {
-            console.log(`Draw! ${humanChoice} and ${computerChoice} are even.`);
+            results.textContent = `Draw! ${humanChoice} and ${computerChoice} are even.`;
         }
     }
     
