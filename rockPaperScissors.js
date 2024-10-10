66e9b7e116090a22023f27369c9459c0f3756563
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2024-10-10 16:51:47 +0100

    rockPaperScissors.js: Made playGame function. Put playRound and event handling inside

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -14,38 +14,42 @@ function getComputerChoice() {
     return choice
 }
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice == "rock" && computerChoice == "scissors") {
-        console.log("You win! Rock beats scissors");
+function playGame() {
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice == "rock" && computerChoice == "scissors") {
+            console.log("You win! Rock beats scissors");
+        }
+        else if (humanChoice == "rock" && computerChoice == "paper") {
+            console.log("You lose! Paper beats rock");
+        }
+        else if (humanChoice == "paper" && computerChoice == "rock") {
+            console.log("You win! Paper beats rock");
+        }
+        else if (humanChoice == "paper" && computerChoice == "scissors") {
+            console.log("You lose! Scissors beats paper");
+        }
+        if (humanChoice == "scissors" && computerChoice == "paper") {
+            console.log("You win! Scissors beats paper");
+        }
+        else if (humanChoice == "scissors" && computerChoice == "rock") {
+            console.log("You lose! Rock beats scissors");
+        }
+        else if (humanChoice == computerChoice) {
+            console.log(`Draw! ${humanChoice} and ${computerChoice} are even.`);
+        }
     }
-    else if (humanChoice == "rock" && computerChoice == "paper") {
-        console.log("You lose! Paper beats rock");
-    }
-    else if (humanChoice == "paper" && computerChoice == "rock") {
-        console.log("You win! Paper beats rock");
-    }
-    else if (humanChoice == "paper" && computerChoice == "scissors") {
-        console.log("You lose! Scissors beats paper");
-    }
-    if (humanChoice == "scissors" && computerChoice == "paper") {
-        console.log("You win! Scissors beats paper");
-    }
-    else if (humanChoice == "scissors" && computerChoice == "rock") {
-        console.log("You lose! Rock beats scissors");
-    }
-    else if (humanChoice == computerChoice) {
-        console.log(`Draw! ${humanChoice} and ${computerChoice} are even.`);
-    }
+    
+    
+    // buttons is a node list. It looks and acts much like an array.
+    const buttons = document.querySelectorAll("button");
+    
+    // we use the .forEach method to iterate through each button
+    buttons.forEach((button) => {
+      // and for each one we add a 'click' listener
+      button.addEventListener("click", () => {
+        playRound(button.id, getComputerChoice())
+    });
+    });
 }
 
-
-// buttons is a node list. It looks and acts much like an array.
-const buttons = document.querySelectorAll("button");
-
-// we use the .forEach method to iterate through each button
-buttons.forEach((button) => {
-  // and for each one we add a 'click' listener
-  button.addEventListener("click", () => {
-    playRound(button.id, getComputerChoice())
-});
-});
+playGame();
