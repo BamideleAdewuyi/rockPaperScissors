69b77670aca850e680cf4694aba7603c611d91f3
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2024-10-10 16:38:09 +0100

    rockPaperScissors.js: removed score changes from playRound

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -17,30 +17,35 @@ function getComputerChoice() {
 function playRound(humanChoice, computerChoice) {
     if (humanChoice == "rock" && computerChoice == "scissors") {
         console.log("You win! Rock beats scissors");
-        humanScore+=1;
     }
     else if (humanChoice == "rock" && computerChoice == "paper") {
         console.log("You lose! Paper beats rock");
-        computerScore+=1;
     }
     else if (humanChoice == "paper" && computerChoice == "rock") {
         console.log("You win! Paper beats rock");
-        humanScore+=1;
     }
     else if (humanChoice == "paper" && computerChoice == "scissors") {
         console.log("You lose! Scissors beats paper");
-        computerScore+=1;
     }
     if (humanChoice == "scissors" && computerChoice == "paper") {
         console.log("You win! Scissors beats paper");
-        humanScore+=1;
     }
     else if (humanChoice == "scissors" && computerChoice == "rock") {
         console.log("You lose! Rock beats scissors");
-        computerScore+=1;
     }
     else if (humanChoice == computerChoice) {
         console.log(`Draw! ${humanChoice} and ${computerChoice} are even.`);
     }
 }
 
+
+// // buttons is a node list. It looks and acts much like an array.
+// const buttons = document.querySelectorAll("button");
+
+// // we use the .forEach method to iterate through each button
+// buttons.forEach((button) => {
+//   // and for each one we add a 'click' listener
+//   button.addEventListener("click", playRound(button.id, getComputerChoice));
+// });
+
+// playRound("rock", "scissors")
